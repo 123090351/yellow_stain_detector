d189436c8692e191cb7d97a399523e452569b1ff
Xabat06 <xabat.es2024@hotmail.com>
2026-07-22 10:01:14 +0800

    freeze

--- scripts/train/stains_yolo.py
+++ scripts/train/stains_yolo.py
@@ -65,7 +65,7 @@ def evaluate_image_level(model, data_dir, conf_thresh=0.175):
 
 
 def run_freeze_optimization():
-    freeze_milestones = [3]
+    freeze_milestones = [0]
     data_path = "/mnt/huangban-data/"
     results_log = {}
     
