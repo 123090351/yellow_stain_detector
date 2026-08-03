54edaf6fef357e9b7ee95a81bbcc386ddd3c251f
Xabat06 <xabat.es2024@hotmail.com>
2026-08-03 08:57:21 +0800

    seed

--- scripts/train/stains_yolo.py
+++ scripts/train/stains_yolo.py
@@ -90,7 +90,7 @@ def run_freeze_optimization():
             patience=30,
             device=0,
             workers=16,
-            seed=100,
+            seed=12345678,
             dropout=0.0,
             
             # Disabled spatial/color augmentations
